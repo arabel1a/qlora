7b613566fb2969e765ba214b2d1e72feff536c48
arabel1a <mscirokikg@yandex.ru>
2026-05-28 14:58:08 +0300

    remove deploy tests :

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rsync -azv ./ bz-ascend:/home/russia_mmo/misha/qlora/
 ssh bz-ascend "docker cp /home/russia_mmo/misha/qlora/all.py va18_misha:/vllm-workspace/vllm-ascend/vllm_ascend/lora/punica_npu.py"
-ssh bz-ascend "docker cp /home/russia_mmo/misha/qlora/test_e2e.py va18_misha:/home/russia_mmo/misha/qlora/test_e2e.py"
-ssh bz-ascend "docker cp /home/russia_mmo/misha/qlora/test_kernels.py va18_misha:/home/russia_mmo/misha/qlora/test_kernels.py"
-echo "--- Running e2e correctness test ---"
-ssh bz-ascend "docker exec va18_misha python /home/russia_mmo/misha/qlora/test_e2e.py --tokens 4 128 2048 --num-loras 2 4 --ranks 16 32 --num-requests 4 16"
+# ssh bz-ascend "docker cp /home/russia_mmo/misha/qlora/test_e2e.py va18_misha:/home/russia_mmo/misha/qlora/test_e2e.py"
+# ssh bz-ascend "docker cp /home/russia_mmo/misha/qlora/test_kernels.py va18_misha:/home/russia_mmo/misha/qlora/test_kernels.py"
+# echo "--- Running e2e correctness test ---"
+# ssh bz-ascend "docker exec va18_misha python /home/russia_mmo/misha/qlora/test_e2e.py --tokens 4 128 2048 --num-loras 2 4 --ranks 16 32 --num-requests 4 16"
